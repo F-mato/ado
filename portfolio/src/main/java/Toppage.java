7e8c5b55754b0f4d0d5a03c24ee07f7e3662fd4c
F-mato <153592419+F-mato@users.noreply.github.com>
2024-02-15 17:48:29 +0900

    Add files via upload

---
+++ portfolio/src/main/java/Toppage.java
@@ -0,0 +1,56 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Toppage {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Connection conn = null;
+        Statement stmt = null;
+        ResultSet rset = null;
+
+        //接続文字列
+        String url = "jdbc:postgresql://localhost:5432/count";
+        String user = "postgres";
+        String password = "admin"; 
+
+        try{
+            //PostgreSQLへ接続
+            conn = DriverManager.getConnection(url, user, password);
+
+            //自動コミットOFF
+            conn.setAutoCommit(false);
+
+            //SELECT文の実行
+            stmt = conn.createStatement();
+            String sql = "SELECT * FROM click_count;";
+            rset = stmt.executeQuery(sql);
+
+            //SELECT結果の受け取り
+            while(rset.next()){
+            	String col1 = rset.getString(1);
+                String col2 = rset.getString(2);
+                System.out.println(col1 + ":" + col2);
+            }
+            conn.commit();
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+            try {
+                if(rset != null)rset.close();
+                if(stmt != null)stmt.close();
+                if(conn != null)conn.close();
+            }
+            catch (SQLException e){
+                e.printStackTrace();
+            }
+
+        }
+	}
+
+}
